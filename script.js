1bbf1afb0e5697cac6695c4643d07a4584c76548
jashwanthakula <jashwanthakula.26@gmail.com>
2024-08-03 16:55:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    document.querySelector(".prev").addEventListener("click", function() {
+        plusSlides(-1);
+    });
+
+    document.querySelector(".next").addEventListener("click", function() {
+        plusSlides(1);
+    });
+
+    document.querySelectorAll(".dot").forEach((dot, index) => {
+        dot.addEventListener("click", function() {
+            currentSlide(index + 1);
+        });
+    });
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+});
+
+function redirectToSuccess() {
+    window.location.href = "success.html";
+}
